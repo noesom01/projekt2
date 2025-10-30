8ab1021b5cd0d69fce4ac084cb4c6381f1bef112
Dit Navn <din@email.com>
2025-10-30 15:36:02 +0100

    rettet h1 og h2 i html dokumenter til at ligge under div. tilføjet media query til mobil og desktop version

---
+++ js/navigation.js
@@ -0,0 +1,11 @@
+const bars = document.querySelector(".bars");
+
+const openNav = () => {
+    document.querySelector("header > nav").
+    classList.toggle("show");
+    document.querySelector("#bars i").classList.toggle("fa-bars");
+    document.querySelector("#bars i").classList.toggle("fa-xmark");
+}
+
+bars.addEventListener("click", openNav);
+
